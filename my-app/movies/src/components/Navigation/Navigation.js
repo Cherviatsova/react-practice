e8a2ba77b4dd9bfa938061123e85fa1f76a5f09b
Cherviatsova <chervyatsovaelena2@gmail.com>
2022-02-21 19:04:58 +0200

    added files

---
+++ my-app/movies/src/components/Navigation/Navigation.js
@@ -0,0 +1,23 @@
+// import { NavLink } from 'react-router-dom';
+import React from 'react';
+import {Link} from './Navigation.styled'
+
+
+export default function Navigation() {
+    return (
+        <nav>
+            <Link
+                to="/"
+                exact
+            >
+                Home
+            </Link>
+
+            <Link
+                to="/movies"
+            >
+                Movies
+            </Link>
+        </nav>
+    );
+}
