504f75581d757e59499761cf989402198263399b
Cherviatsova <chervyatsovaelena2@gmail.com>
2022-05-11 20:09:40 +0300

    test

---
+++ my-app/phonebook/src/App.js
@@ -0,0 +1,21 @@
+import { AppContainer, Title } from "./App.styled";
+import Form from "./components/Form/Form";
+import Filter from "./components/Filter/Filter";
+import ContactsList from "./components/ContactsList/ContactsList";
+
+
+
+export default function App () {
+  
+ 
+    return (
+      <AppContainer>
+        <Title>Phonebook</Title>
+        <Form/>
+        <Title> Contacts </Title>
+        <Filter/>
+        <ContactsList/>
+      </AppContainer>
+    );
+}
+
